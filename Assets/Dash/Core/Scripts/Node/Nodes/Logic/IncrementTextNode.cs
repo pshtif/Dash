529654fb71fbcc0cab00de67a080e33f6732339f
shtif <pshtif@gmail.com>
2021-02-14 19:09:27 +0100

    added IncrementTextNode

---
+++ Assets/Dash/Core/Scripts/Node/Nodes/Logic/IncrementTextNode.cs
@@ -0,0 +1,57 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using System;
+using System.Globalization;
+using Dash.Attributes;
+using TMPro;
+using UnityEngine;
+
+namespace Dash
+{
+    [Category(NodeCategoryType.LOGIC)]
+    [InputCount(1)]
+    [OutputCount(1)]
+    public class IncrementTextNode : RetargetNodeBase<IncrementTextNodeModel>
+    {
+        protected override void ExecuteOnTarget(Transform p_target, NodeFlowData p_flowData)
+        {
+            TMP_Text tmp = p_target.GetComponent<TMP_Text>();
+
+            if (CheckException(tmp, "Target doesn't contain TMP_Text component."))
+                return;
+
+            string text = tmp.text;
+            if (Model.useDotFormating)
+            {
+                text = text.Replace(".", "");
+            }
+
+            int value;
+            if (!Int32.TryParse(text, out value))
+            {
+                Debug.LogWarning("Not a valid Int value in target text in node "+Model.id);
+                return;
+            }
+
+            value += Model.increment;
+            text = value.ToString();
+
+            if (Model.useDotFormating)
+            {
+                int i = text.Length;
+                while (i > 3)
+                {
+                    i -= 3;
+                    text = text.Insert(i, ".");
+                }
+            }
+
+            tmp.text = text;
+            
+            OnExecuteOutput(0, p_flowData);
+            OnExecuteEnd();
+        }
+    }
+}
---
+++ Assets/Dash/Core/Scripts/Node/Nodes/Logic/IncrementTextNodeModel.cs
@@ -0,0 +1,12 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+namespace Dash
+{
+    public class IncrementTextNodeModel : RetargetNodeModel
+    {
+        public bool useDotFormating = true;
+        public int increment = 1;
+    }
+}
