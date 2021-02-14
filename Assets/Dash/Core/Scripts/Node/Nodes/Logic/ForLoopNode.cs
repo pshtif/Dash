4471d73591efeae81ad385bd1630d4735e481694
shtif <pshtif@gmail.com>
2021-02-14 19:10:18 +0100

    add delay options for forloop node

--- Assets/Dash/Core/Scripts/Node/Nodes/Logic/ForLoopNode.cs
+++ Assets/Dash/Core/Scripts/Node/Nodes/Logic/ForLoopNode.cs
@@ -10,7 +10,7 @@ namespace Dash
     [Category(NodeCategoryType.LOGIC)]
     [OutputCount(2)]
     [InputCount(1)]
-    [OutputLabels("OnItem", "OnFinished")]
+    [OutputLabels("OnIteration", "OnFinished")]
     public class ForLoopNode : NodeBase<ForLoopNodeModel>
     {
         protected override void OnExecuteStart(NodeFlowData p_flowData)
@@ -18,17 +18,38 @@ namespace Dash
             int firstIndex = Model.firstIndex.GetValue(ParameterResolver, p_flowData);
             int lastIndex = Model.lastIndex.GetValue(ParameterResolver, p_flowData);
             int length = lastIndex - firstIndex;
+            
             if (length == 0)
                 EndLoop(p_flowData);
 
             for (int i = firstIndex; i != lastIndex; i += Math.Abs(length) / length)
             {
-                p_flowData.SetAttribute(Model.indexVariable, i);
-                OnExecuteOutput(0, p_flowData);
+                NodeFlowData data = p_flowData.Clone();
+                data.SetAttribute(Model.indexVariable, i);
+                if (Model.OnIterationDelay.GetValue(ParameterResolver) == 0)
+                {
+                    OnExecuteOutput(0, data);
+                }
+                else
+                {
+                    DOPreview.DelayedCall(Model.OnIterationDelay.GetValue(ParameterResolver) * i, () =>
+                    {
+                        OnExecuteOutput(0, data);
+                    });
+                }
             }
             
-            OnExecuteEnd();
-            OnExecuteOutput(1, p_flowData);
+            if (Model.OnFinishedDelay == 0 && Model.OnIterationDelay.GetValue(ParameterResolver) == 0)
+            {
+                EndLoop(p_flowData);
+            }
+            else
+            {
+                DOPreview.DelayedCall(Model.OnFinishedDelay + Model.OnIterationDelay.GetValue(ParameterResolver) * length, () =>
+                {
+                    EndLoop(p_flowData);
+                });
+            }
         }
 
         void EndLoop(NodeFlowData p_flowData)
--- Assets/Dash/Core/Scripts/Node/Nodes/Logic/ForLoopNodeModel.cs
+++ Assets/Dash/Core/Scripts/Node/Nodes/Logic/ForLoopNodeModel.cs
@@ -3,11 +3,17 @@
  */
 
 using Dash.Attributes;
+using UnityEngine;
 
 namespace Dash
 {
     public class ForLoopNodeModel : NodeModelBase
     {
+        [Tooltip("Delay in execution after each iteration.")]
+        public Parameter<float> OnIterationDelay = new Parameter<float>(0);
+        [Tooltip("Delay in execution after last child executed.")]
+        public float OnFinishedDelay = 0;
+        
         public Parameter<int> firstIndex = new Parameter<int>(0);
         public Parameter<int> lastIndex = new Parameter<int>(0);
 
