01878f87b6fe48f67574ded5b06105b8859c776b
shtif <pshtif@gmail.com>
2021-02-15 11:24:08 +0100

    misc project files update

---
+++ Assets/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Dash;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestController : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public int type = 0;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NodeFlowData data = NodeFlowDataFactory.Create();
+            
+            Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RectTransform rect = GetComponent<RectTransform>();
+            Vector2 point;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, mousePos, Camera.main, out point);
+            data.SetAttribute<Vector2>("spawnPosition", point);
+            
+            RectTransform r = transform.GetChild(type).GetComponent<RectTransform>();
+            data.SetAttribute<Vector2>("toPosition", TransformUtils.FromToRectTransform(r, rect));
+            
+            data.SetAttribute<int>("type", type);
+            
+            data.SetAttribute<Sprite>("sprite", sprites[type]);
+            DashCore.Instance.SendEvent("Click", data);
+        }    
+    }
+}
