e3fecf2bfa8af8befdaeaef901f02ce81f188f8a
shtif <pshtif@gmail.com>
2021-03-08 10:36:11 +0100

    update version

--- Assets/Dash/Core/Scripts/DashEditorCore.cs
+++ Assets/Dash/Core/Scripts/DashEditorCore.cs
@@ -15,7 +15,7 @@ namespace Dash
     [InitializeOnLoad]
     public class DashEditorCore
     {
-        public const string VERSION = "0.2.4b";
+        public const string VERSION = "0.2.5b";
 
         static public DashEditorConfig Config { get; private set; }
 
