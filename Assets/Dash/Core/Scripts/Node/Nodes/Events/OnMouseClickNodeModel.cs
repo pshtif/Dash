d76a6e09eae2c0e45f1217ace05ddae1faa85f7d
shtif <pshtif@gmail.com>
2021-03-10 23:23:51 +0100

    added onmouseclick node

--- Assets/Dash/Core/Scripts/Node/Nodes/Events/OnMouseClickNodeModel.cs
+++ Assets/Dash/Core/Scripts/Node/Nodes/Events/OnMouseClickNodeModel.cs
@@ -2,41 +2,10 @@
  *	Created by:  Peter @sHTiF Stefcek
  */
 
-using Dash.Attributes;
-using UnityEngine;
-using UnityEngine.UI;
-
 namespace Dash
 {
     public class OnMouseClickNodeModel : NodeModelBase
     {
-        [Tooltip("Use scene reference as retarget.")]
-        public bool useReference = false;
-        
-        [Dependency("retarget", true)]
-        [Dependency("useReference", false)]
-        [Tooltip("Retarget to child of controller.")]
-        public bool isChild;
-        
-        [Dependency("useReference", false)]
-        [Tooltip("Lookup for name match instead of path.")]
-        public bool useFind;
-        
-        [Dependency("useReference", false)]
-        [Dependency("useFind", true)]
-        [Tooltip("Look up for all instances.")]
-        public bool findAll;
-        
-        [Dependency("retarget", true)]
-        [Dependency("useReference", false)]
-        [Tooltip("Name of a button or child if relative, use / for hierachy.")]
-        public string button;
-        
-        [Dependency("retarget", true)]
-        [Dependency("useReference", true)]
-        [Tooltip("Reference of a button.")]
-        public ExposedReference<Button> buttonReference;
-
-        public bool retargetToButton = true;
+        public bool transformToLocal = true;
     }
 }
