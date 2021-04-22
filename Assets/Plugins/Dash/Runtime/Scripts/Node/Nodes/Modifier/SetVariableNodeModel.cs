7ae4780ac32be5fe763080614b4c701e95589f5c
sHTiF <pshtif@gmail.com>
2021-04-22 23:27:21 +0200

    add SetVariableNode

---
+++ Assets/Plugins/Dash/Runtime/Scripts/Node/Nodes/Modifier/SetVariableNodeModel.cs
@@ -0,0 +1,17 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using System;
+
+namespace Dash
+{
+    [Serializable]
+    public class SetVariableNodeModel : NodeModelBase
+    {
+        public string variableName;
+        public Type attributeType = typeof(int);
+        public string expression;
+        public bool enableCreate = false;
+    }
+}
