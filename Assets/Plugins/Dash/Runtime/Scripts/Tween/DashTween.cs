a638b71507e0623f445bff66fa8c7685ef3371fb
sHTiF <pshtif@gmail.com>
2021-04-23 17:27:47 +0200

    update DashTween

--- Assets/Plugins/Dash/Runtime/Scripts/Tween/DashTween.cs
+++ Assets/Plugins/Dash/Runtime/Scripts/Tween/DashTween.cs
@@ -2,10 +2,146 @@
  *	Created by:  Peter @sHTiF Stefcek
  */
 
+using System;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+
 namespace Dash
 {
-    public class DashTween
+    public class DashTween : IInternalTweenAccess
     {
+        static internal List<DashTween> _activeTweens = new List<DashTween>();
+
+        static private Queue<DashTween> _pooledTweens = new Queue<DashTween>();
         
+        public Object target { get; private set; }
+        public float delay { get; private set; }
+        public float from { get; private set; }
+        public float to { get; private set; }
+        public float duration { get; private set; }
+
+        public EaseType easeType = EaseType.LINEAR;
+        
+        public float current { get; private set; }
+
+        public bool running { get; private set; } = false;
+
+        private Action<float> _updateCallback;
+        private Action _completeCallback;
+
+        public static DashTween To(Object p_target, float p_from, float p_to, float p_time)
+        {
+            DashTweenCore.Initialize();
+            
+            DashTween tween = DashTween.Create(p_target, p_from, p_to, p_time);
+            return tween;
+        }
+
+        private static DashTween Create(Object p_target, float p_from, float p_to, float p_duration)
+        {
+            DashTween tween;
+            if (_pooledTweens.Count == 0)
+            {
+                tween = new DashTween();
+            }
+            else
+            {
+                tween = _pooledTweens.Dequeue();
+                tween.current = 0;
+                tween.running = false;
+                tween.delay = 0;
+            }
+            
+            tween.target = p_target;
+            tween.from = p_from;
+            tween.to = p_to;
+            tween.duration = p_duration;
+            
+            _activeTweens.Add(tween);
+
+            return tween;
+        }
+
+        public DashTween SetDelay(float p_delay)
+        {
+            delay = p_delay;
+            return this;
+        }
+
+        public DashTween SetEase(EaseType p_easeType)
+        {
+            easeType = p_easeType;
+            return this;
+        }
+
+        public DashTween OnUpdate(Action<float> p_callback)
+        {
+            _updateCallback = p_callback;
+            return this;
+        }
+
+        public DashTween OnComplete(Action p_callback)
+        {
+            _completeCallback = p_callback;
+            return this;
+        }
+
+        public void Start()
+        {
+            if (duration == 0 && delay == 0)
+            {
+                _updateCallback?.Invoke(EaseValue(from, to, current/(duration-delay), easeType));
+                _completeCallback?.Invoke();
+                Clean();
+            }
+            else
+            {
+                running = true;
+            }
+        }
+
+        public void Kill(bool p_complete)
+        {
+            // TODO Implement
+        }
+
+        bool IInternalTweenAccess.Update(float p_time)
+        {
+            if (!running)
+                return false;
+            
+            current += p_time;
+            if (current > duration + delay)
+            {
+                current = duration+delay;
+                _updateCallback?.Invoke(EaseValue(from, to, current/(duration-delay), easeType));
+                _completeCallback?.Invoke();
+                Clean();
+                return true;
+            }
+            else
+            {
+                _updateCallback?.Invoke(EaseValue(from, to, current/(duration-delay), easeType));
+                return false;
+            }
+        }
+
+        void Clean()
+        {
+            running = false;
+            _activeTweens.Remove(this);
+            _pooledTweens.Enqueue(this);
+        }
+
+        internal static void CleanAll()
+        {
+            _activeTweens.Clear();
+            _pooledTweens.Clear();
+        }
+        
+        public static float EaseValue(float p_from, float p_to, float p_delta, EaseType p_easing)
+        {
+            return p_from + (p_to - p_from) * EaseFunctions.Evaluate(p_delta, 1f, p_easing);
+        }
     }
 }
--- Assets/Plugins/Dash/Runtime/Scripts/Tween/DashTweenCore.cs
+++ Assets/Plugins/Dash/Runtime/Scripts/Tween/DashTweenCore.cs
@@ -2,22 +2,78 @@
  *	Created by:  Peter @sHTiF Stefcek
  */
 
+using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Dash
 {
-    public class DashTween : MonoBehaviour
+    public class DashTweenCore : MonoBehaviour
     {
-        private static DashTween _instance;
-        
+        [NonSerialized]
+        private static bool _initialized = false;
+
         public static void Initialize()
         {
-            Debug.Log("Initialize Dash Tween Core");
-            _instance = new GameObject();
-            _instance.name = "DashTweenCore";
-            _instance.hideFlags = HideFlags.HideAndDontSave;
-            _core.AddComponent<TweenCore>();
-            GameObject.DontDestroyOnLoad(_core);
+            if (!_initialized)
+            {
+                if (Application.isPlaying)
+                {
+                    var go = new GameObject();
+                    go.name = "DashTweenCore";
+                    go.hideFlags = HideFlags.HideAndDontSave;
+                    go.AddComponent<DashTweenCore>();
+                    DontDestroyOnLoad(go);
+                }
+                else
+                {
+                    #if UNITY_EDITOR
+                    _currentTime = EditorApplication.timeSinceStartup;
+                    EditorApplication.update -= UpdateEditor;
+                    EditorApplication.update += UpdateEditor;
+                    #endif
+                }
+
+                _initialized = true;
+            }
         }
+
+        void Update()
+        {
+            UpdateInternal(Time.deltaTime);
+        }
+
+        static void UpdateInternal(float p_delta)
+        {
+            for (int i = 0; i < DashTween._activeTweens.Count; i++)
+            {
+                if (((IInternalTweenAccess) DashTween._activeTweens[i]).Update(p_delta))
+                {
+                    // In place removing of completed tweens
+                    i--;
+                }
+            }
+        }
+        
+        #if UNITY_EDITOR
+        internal static void Reset()
+        {
+            _initialized = false;
+            DashTween.CleanAll();
+        }
+        
+        private static double _currentTime = 0;
+        
+        private static void UpdateEditor()
+        {
+            float delta = (float) (EditorApplication.timeSinceStartup - _currentTime);
+            
+            UpdateInternal(delta);
+            
+            _currentTime = EditorApplication.timeSinceStartup;
+        }
+        #endif
     }
 }
