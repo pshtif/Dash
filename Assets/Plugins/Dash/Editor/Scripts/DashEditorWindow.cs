5859430cce6c1a01bce0a2147407c94dde433fe3
sHTiF <pshtif@gmail.com>
2021-04-30 22:42:49 +0200

    fix no migration for empty graphs

--- Assets/Plugins/Dash/Editor/Scripts/DashEditorWindow.cs
+++ Assets/Plugins/Dash/Editor/Scripts/DashEditorWindow.cs
@@ -99,31 +99,41 @@ namespace Dash
 
         private bool CheckVersionUI()
         {
-            if (DashEditorCore.Config.editingGraph != null &&
-                DashEditorCore.Config.editingGraph.version < DashEditorCore.GetVersionNumber())
+            if (DashEditorCore.Graph != null &&
+                DashEditorCore.Graph.version < DashEditorCore.GetVersionNumber())
             {
-                GUIStyle style = DashEditorCore.Skin.GetStyle("ViewBase");
-                var rect = new Rect(position.width / 2 - 160, position.height / 2 - 70, 320, 160);
-                GUI.Box(rect, new GUIContent("Version Warning"), style);
-                GUILayout.BeginArea(new Rect(rect.x + 6, rect.y+34, rect.width -12, rect.height-36));
-                GUILayout.BeginVertical();
-                GUIStyle textStyle = new GUIStyle();
-                textStyle.normal.textColor = Color.white;
-                textStyle.wordWrap = true;
-                textStyle.alignment = TextAnchor.UpperCenter;
-                GUILayout.TextArea("This graph was created by previous version of Dash Animation System version: "+DashEditorCore.Config.editingGraph.version+"\n"+
-                                   "The current version is "+DashEditorCore.GetVersionString(DashEditorCore.GetVersionNumber())+
-                                   " so it is needed to migrate and revalidate serialization or the Graph may not work correctly.\n"+
-                                   "Make sure you have backup of this Graph.", textStyle);
-                GUILayout.Space(4);
-                if (GUILayout.Button("Migrate"))
+                if (DashEditorCore.Graph.Nodes.Count == 0)
                 {
                     DashEditorCore.Graph.ValidateSerialization();
                 }
-                GUILayout.EndVertical();
-                GUILayout.EndArea();
+                else
+                {
+                    GUIStyle style = DashEditorCore.Skin.GetStyle("ViewBase");
+                    var rect = new Rect(position.width / 2 - 160, position.height / 2 - 70, 320, 160);
+                    GUI.Box(rect, new GUIContent("Version Warning"), style);
+                    GUILayout.BeginArea(new Rect(rect.x + 6, rect.y + 34, rect.width - 12, rect.height - 36));
+                    GUILayout.BeginVertical();
+                    GUIStyle textStyle = new GUIStyle();
+                    textStyle.normal.textColor = Color.white;
+                    textStyle.wordWrap = true;
+                    textStyle.alignment = TextAnchor.UpperCenter;
+                    GUILayout.TextArea("This graph was created by previous version of Dash Animation System version: " +
+                                       DashEditorCore.Config.editingGraph.version + "\n" +
+                                       "The current version is " +
+                                       DashEditorCore.GetVersionString(DashEditorCore.GetVersionNumber()) +
+                                       " so it is needed to migrate and revalidate serialization or the Graph may not work correctly.\n" +
+                                       "Make sure you have backup of this Graph.", textStyle);
+                    GUILayout.Space(4);
+                    if (GUILayout.Button("Migrate"))
+                    {
+                        DashEditorCore.Graph.ValidateSerialization();
+                    }
 
-                return false;
+                    GUILayout.EndVertical();
+                    GUILayout.EndArea();
+
+                    return false;
+                }
             }
 
             return true;
