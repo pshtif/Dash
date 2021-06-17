aed48c414f16a3b163dc6222bae7d081a875255a
Peter <pshtif@gmail.com>
2021-06-17 18:29:54 +0200

    properties ordering updated

--- Assets/Plugins/Dash/Runtime/Scripts/Node/Nodes/Common/RetargetNodeModelBase.cs
+++ Assets/Plugins/Dash/Runtime/Scripts/Node/Nodes/Common/RetargetNodeModelBase.cs
@@ -16,21 +16,25 @@ namespace Dash
     [Serializable]
     public class RetargetNodeModelBase : NodeModelBase
     {
+        [Order(0)]
         [TitledGroup("Retargeting", -1)]
         [Tooltip("Change current target.")]
         public bool retarget = false;
 
+        [Order(1)]
         [TitledGroup("Retargeting", -1)]
         [Dependency("retarget", true)]
         [Tooltip("Use scene reference as retarget.")]
         public bool useReference = false;
         
+        [Order(2)]
         [TitledGroup("Retargeting", -1)]
         [Dependency("retarget", true)]
         [Dependency("useReference", false)]
         [Tooltip("Retarget to child of current.")]
         public bool isChild;
         
+        [Order(3)]
         [TitledGroup("Retargeting", -1)]
         [Dependency("retarget", true)]
         [Dependency("useReference", false)]
@@ -38,13 +42,14 @@ namespace Dash
         [CanBeNull]
         public Parameter<string> target = new Parameter<string>("");
 
+        [Order(4)]
         [TitledGroup("Retargeting", -1)]
         [Expression("useExpression", "targetExpression")]
         [Dependency("retarget", true)]
         [Dependency("useReference", true)]
         [Tooltip("Reference of transform to retarget to.")]
         public ExposedReference<Transform> targetReference;
-
+        
         [TitledGroup("Retargeting", -1)]
         [HideInInspector]
         public bool useExpression = false;
