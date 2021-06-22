b994165d58cc7c21a04968134b9a9a899e6e6d5c
Peter <pshtif@gmail.com>
2021-06-22 22:39:10 +0200

    changed ordering

--- Assets/Plugins/Dash/Runtime/Scripts/Node/Nodes/Modifier/RetargetToChildrenNodeModel.cs
+++ Assets/Plugins/Dash/Runtime/Scripts/Node/Nodes/Modifier/RetargetToChildrenNodeModel.cs
@@ -9,12 +9,15 @@ namespace Dash
 {
     public class RetargetToChildrenNodeModel : RetargetNodeModelBase
     {
+        [Order(10)]
         [Tooltip("Delay in execution after each child.")]
         public Parameter<float> onChildDelay = new Parameter<float>(0);
         
+        [Order(11)]
         [Tooltip("Delay in execution after last child executed.")]
         public float onFinishDelay = 0;
         
+        [Order(12)]
         [Tooltip("Iterates child in reverse order.")]
         public bool inReverse = false;
     }
