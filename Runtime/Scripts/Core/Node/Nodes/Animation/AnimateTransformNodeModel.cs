2815847a8d8b66978caff6b467da7079e1bdd462
Peter <pshtif@gmail.com>
2021-12-14 20:39:21 +0100

    added AnimateTransformNode

---
+++ Runtime/Scripts/Core/Node/Nodes/Animation/AnimateTransformNodeModel.cs
@@ -0,0 +1,45 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using System;
+using Dash.Attributes;
+using UnityEngine;
+
+namespace Dash
+{
+    [Serializable]
+    public class AnimatePositionNodeModel : AnimationNodeModelBase
+    {
+        [Order(11)]
+        [TitledGroup("Properties")]
+        public bool useFrom = false;
+        
+        [Order(12)]
+        [Dependency("useFrom", true)]
+        [TitledGroup("Properties")]
+        public Parameter<Vector3> fromPosition = new Parameter<Vector3>(Vector3.zero);
+        
+        [Order(13)]
+        [Dependency("useFrom", true)]
+        [TitledGroup("Properties")]
+        public bool isFromRelative = false;
+        
+        [Order(14)]
+        [TitledGroup("Properties")]
+        public Parameter<Vector3> toPosition = new Parameter<Vector3>(Vector3.zero);
+        
+        [Order(15)]
+        [TitledGroup("Properties")]
+        public bool isToRelative = false;
+        
+        [Order(19)]
+        [TitledGroup("Properties")]
+        public bool storeToAttribute = false;
+        
+        [Order(20)]
+        [TitledGroup("Properties")]
+        [Dependency("storeToAttribute", true)]
+        public Parameter<string> storeAttributeName = new Parameter<string>("position");
+    }
+}
