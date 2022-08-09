d0d75cebfc67e4d7ae535d7105a911600489499f
Peter <pshtif@gmail.com>
2022-08-09 19:07:53 +0200

    parametrize properties

--- Runtime/Scripts/Core/Node/Nodes/Modifier/RetargetToChildrenNodeModel.cs
+++ Runtime/Scripts/Core/Node/Nodes/Modifier/RetargetToChildrenNodeModel.cs
@@ -18,12 +18,26 @@ namespace Dash
         [Order(11)]
         [UnityEngine.Tooltip("Delay in execution after last child executed.")]
         [TitledGroup("Properties")]
+        [HideInInspector]
         public float onFinishDelay = 0;
-        
+
+        [Order(11)]
+        [UnityEngine.Tooltip("Delay in execution after last child executed.")]
+        [TitledGroup("Properties")]
+        [Label("onFinishDelay")]
+        public Parameter<float> onFinishDelayP = new Parameter<float>(0);
+
         [Order(12)]
         [UnityEngine.Tooltip("Iterates child in reverse order.")]
         [TitledGroup("Properties")]
+        [HideInInspector]
         public bool inReverse = false;
+
+        [Order(12)]
+        [UnityEngine.Tooltip("Iterates child in reverse order.")]
+        [TitledGroup("Properties")]
+        [Label("inReverse")]
+        public Parameter<bool> inReverseP = new Parameter<bool>(false);
         
         [Order(13)]
         [UnityEngine.Tooltip("Iterates child that are inactive.")]
@@ -37,6 +51,16 @@ namespace Dash
             if (targetInactive == null)
             {
                 targetInactive = new Parameter<bool>(true);
+            }
+            
+            if (onFinishDelayP == null)
+            {
+                onFinishDelayP = new Parameter<float>(onFinishDelay);
+            }
+            
+            if (inReverseP == null)
+            {
+                inReverseP = new Parameter<bool>(inReverse);
             }
 #pragma warning restore 612, 618 
         }
