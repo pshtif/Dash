ce7caaeba85f946c6c3dba4a5a8f2de6b3c5ed09
Peter <pshtif@gmail.com>
2022-11-01 12:50:52 +0100

    refactor editor utils

---
+++ Editor/Scripts/Utils/GUIEditorUtils.cs
@@ -0,0 +1,54 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using UnityEngine;
+
+namespace Dash.Editor
+{
+    public class GUIUtils
+    {
+        public static void DrawTitle(string p_title)
+        {
+            var titleStyle = new GUIStyle();
+            titleStyle.alignment = TextAnchor.MiddleCenter;
+            titleStyle.normal.textColor = new Color(1, .7f, 0);
+            titleStyle.normal.background = Texture2D.whiteTexture;
+            titleStyle.fontStyle = FontStyle.Bold;
+            titleStyle.fontSize = 16;
+
+            GUI.backgroundColor = new Color(0, 0, 0, .35f);
+            
+            GUILayout.Label(p_title, titleStyle, GUILayout.ExpandWidth(true), GUILayout.Height(30));
+            GUILayout.Space(4);
+
+            GUI.backgroundColor = Color.white;
+        }
+        
+        public static bool DrawMinimizableSectionTitle(string p_title, ref bool p_minimized)
+        {
+            var style = new GUIStyle();
+            style.normal.textColor = new Color(1, 0.7f, 0);
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontStyle = FontStyle.Bold;
+            style.normal.background = Texture2D.whiteTexture;
+            style.fontSize = 16;
+            GUI.backgroundColor = new Color(0, 0, 0, .5f);
+
+            GUILayout.Label(p_title, style, GUILayout.Height(26));
+            var rect = GUILayoutUtility.GetLastRect();
+
+            style = new GUIStyle();
+            style.fontSize = 20;
+            
+            GUI.Label(new Rect(rect.x+rect.width- (p_minimized ? 24 : 21), rect.y, 24, 24), p_minimized ? "+" : "-", style);
+            
+            if (GUI.Button(new Rect(rect.x, rect.y, rect.width, rect.height), "", GUIStyle.none))
+            {
+                p_minimized = !p_minimized;
+            }
+
+            return !p_minimized;
+        }
+    }
+}
