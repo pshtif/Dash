f67fdc23b561a6df521dc8248b62feb9df1344a7
Peter <pshtif@gmail.com>
2022-11-14 17:06:13 +0100

    add suffix option for text shortening

--- Runtime/Scripts/Core/Utils/GUIUtils.cs
+++ Runtime/Scripts/Core/Utils/GUIUtils.cs
@@ -8,16 +8,23 @@ namespace Dash
 {
     public class GUIUtils
     {
-        public static string GetMaxLabelForStyleAndWidth(GUIStyle p_style, float p_width, string p_label)
+        public static string GetMaxTextForStyleAndWidth(GUIStyle p_style, float p_width, string p_label, string p_suffix = "")
         {
-            Vector2 size = p_style.CalcSize(new GUIContent(p_label));
-            while (p_label.Length > 0 && size.x > p_width)
+            var maxLabel = p_label;
+            Vector2 size = p_style.CalcSize(new GUIContent(maxLabel));
+            while (maxLabel.Length > 0 && size.x > p_width)
             {
-                p_label = p_label.Substring(0, p_label.Length - 1);
-                size = p_style.CalcSize(new GUIContent(p_label));
+                maxLabel = maxLabel.Substring(0, maxLabel.Length - 1);
+                size = p_style.CalcSize(new GUIContent(maxLabel));
+                
             }
 
-            return p_label;
+            if (maxLabel != p_label)
+            {
+                maxLabel += p_suffix;
+            }
+
+            return maxLabel;
         }
     }
 }
