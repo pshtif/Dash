eb2fdec5f8fff48d61214178c847544659cf8cb1
Peter <pshtif@gmail.com>
2022-12-04 22:43:50 +0100

    update version

--- Runtime/Scripts/Core/DashCore.cs
+++ Runtime/Scripts/Core/DashCore.cs
@@ -14,7 +14,7 @@ namespace Dash
 {
     public class DashCore
     {
-        public const string VERSION = "0.13.6";
+        public const string VERSION = "0.13.7";
         
         public DashRuntimeConfig Config { get; private set; }
         
