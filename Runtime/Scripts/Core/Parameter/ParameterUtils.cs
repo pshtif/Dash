2a4772ea5b0061a4a272b78b1acd5170f9dd2d74
Peter <pshtif@gmail.com>
2022-12-16 15:40:37 +0100

    add ParameterUtils

---
+++ Runtime/Scripts/Core/Parameter/ParameterUtils.cs
@@ -0,0 +1,31 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using UnityEngine;
+
+namespace Dash
+{
+    public class ParameterUtils
+    {
+        public static void MigrateParameter<T>(ref Parameter<T> p_parameterFrom, ref Parameter<T> p_parameterTo)
+        {
+            if (p_parameterTo == null)
+            {
+                p_parameterTo = new Parameter<T>(default(T));
+                
+                if (p_parameterFrom != null)
+                {
+                    if (p_parameterFrom.isExpression)
+                    {
+                        p_parameterTo.expression = p_parameterFrom.expression;
+                    }
+                    else
+                    {
+                        p_parameterTo.SetValue(p_parameterFrom.GetValue(null));
+                    }
+                }
+            }
+        } 
+    }
+}
