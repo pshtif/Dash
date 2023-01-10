498181e8a88ff8a018340187526e8d8668c8886c
Peter <pshtif@gmail.com>
2023-01-10 16:11:18 +0100

    add DashGraphCache

---
+++ Runtime/Scripts/Graph/DashGraphCache.cs
@@ -0,0 +1,48 @@
+/*
+ *	Created by:  Peter @sHTiF Stefcek
+ */
+
+using System.Collections.Generic;
+using OdinSerializer;
+using UnityEngine;
+
+namespace Dash
+{
+    public class DashGraphCache
+    {
+        private static Dictionary<DashGraph, SerializedValue> _serializedGraphs =
+            new Dictionary<DashGraph, SerializedValue>();
+        
+        public static DashGraph GetInstance(DashGraph p_graphAsset)
+        {
+            if (!_serializedGraphs.ContainsKey(p_graphAsset))
+            {
+                _serializedGraphs.Add(p_graphAsset, SerializeGraph(p_graphAsset));
+            }
+
+            return DeserializeGraph(_serializedGraphs[p_graphAsset]);
+        }
+        
+        public static SerializedValue SerializeGraph(DashGraph p_graph, DataFormat p_format = DataFormat.Binary)
+        {
+            SerializedValue serialized = new SerializedValue();
+            serialized.bytes = p_graph.SerializeToBytes(p_format, ref serialized.references);
+            
+            return serialized;
+        }
+
+        public static DashGraph DeserializeGraph(SerializedValue p_value)
+        {
+            DashGraph graph = ScriptableObject.CreateInstance<DashGraph>();
+            p_value.references[0] = graph;
+            
+            graph.DeserializeFromBytes(p_value.bytes, DataFormat.Binary, ref p_value.references);
+            return graph;
+        }
+
+        public static void ReleaseCache()
+        {
+            _serializedGraphs = new Dictionary<DashGraph, SerializedValue>();
+        }
+    }
+}
