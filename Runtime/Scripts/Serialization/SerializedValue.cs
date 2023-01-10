8efa0453a42b8f11079493b2ae5022658c96d758
Peter <pshtif@gmail.com>
2023-01-10 16:12:45 +0100

    update SerializedValue with data format

--- Runtime/Scripts/Serialization/SerializedValue.cs
+++ Runtime/Scripts/Serialization/SerializedValue.cs
@@ -4,18 +4,16 @@
 
 using System;
 using System.Collections.Generic;
+using OdinSerializer;
+using Object = UnityEngine.Object;
 
 namespace Dash
 {
     [Serializable]
     public class SerializedValue
     {
+        public DataFormat format = DataFormat.Binary;
         public byte[] bytes;
-        public List<UnityEngine.Object> references;
-
-        public SerializedValue()
-        {
-            references = new List<UnityEngine.Object>();
-        }
+        public List<UnityEngine.Object> references = new List<Object>();
     }
 }
